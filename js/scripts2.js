188c39acf394589f0021491bb19addcc57ad0dd5
dahlia rizk <76135661+dahliarizk@users.noreply.github.com>
2021-03-27 13:57:18 -0400

    meets expectations

--- js/scripts2.js
+++ js/scripts2.js
@@ -4,7 +4,7 @@ let header = document.querySelector('.header-text-container');
 
 //fetch request to return data from randomuser API, and return people array of objects
 //then calls createCard function to build one card per people[i] object
-fetch ('https://randomuser.me/api/?results=12&nat=us,dk,fr,gb,ca,au,nz,de')
+fetch ('https://randomuser.me/api/?results=12&nat=us')
     .then(response => response.json())
     .then(data => {
       let people = data.results
@@ -19,24 +19,9 @@ fetch ('https://randomuser.me/api/?results=12&nat=us,dk,fr,gb,ca,au,nz,de')
           cards.forEach(card => card.addEventListener('click', (event) => {
               updateModalWindow(people, event)
           }));
-          document.addEventListener('click', e => {
-            if (e.target.textContent === 'Prev'){
-              //prevProfile function
-            }
-          })
-          //searchFunction goes here
-      });
-  //.catch(error => console.log('There is an issue', error));
+      })
+  .catch(error => console.log('There is an issue', error));
 
-
-
-//creates searchBar form and button. to be built on later.
-let searchBar = document.querySelector('.search-container')
-searchBar.innerHTML = `
-    <form action="#" method="get">
-        <input type="search" id="search-input" class="search-input" placeholder="Search...">
-        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
-    </form>`
 
 let input = document.querySelectorAll('input')[0];
 let searchImg = document.querySelectorAll('input')[1];
@@ -58,47 +43,45 @@ function createCard(people){
       </div>`)
     }
     return people;
-  }
-
-  function addModalWindow(){
-      gallery.insertAdjacentHTML('afterend', `
-      <div class="modal-container">
-          <div class="modal">
-              <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-              <div class="modal-info-container">
-              </div>
-          </div>
-          <div class="modal-btn-container">
-              <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
-              <button type="button" id="modal-next" class="modal-next btn">Next</button>
-          </div>
-      </div>`);
-      let modalWindow = document.querySelector('.modal-container');
-      modalWindow.style.display = 'none';
   };
 
+//function to add HTML elements of modal window that do not change
+function addModalWindow(){
+    gallery.insertAdjacentHTML('afterend', `
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+            </div>
+        </div>
+    </div>`);
+    let modalWindow = document.querySelector('.modal-container');
+    modalWindow.style.display = 'none';
+};
 
-function updateModalWindow(people){
+//updates modal window content using people array. compares event target of the click with each of the objects and if they match
+//displays corresponding object.
+function updateModalWindow(people, event){
   let modalWindow = document.querySelector('.modal-container');
   modalWindow.style.display = 'block'
+  console.log(event.target.className)
   for (let i = 0; i < people.length; i++){
     if (event.target.textContent === `${people[i].name.last} ${people[i].name.first}`
           || event.target.textContent === `${people[i].email}`
           || event.target.textContent === `${people[i].location.city}, ${people[i].location.state}`
-          || event.target.src === `${people[i].picture.thumbnail}` || event.target.type === 'button'){
-          let modalInfo = document.querySelector('.modal-info-container');
-          modalInfo.innerHTML = '';
-          console.log(i)
-          modalInfo.insertAdjacentHTML('afterbegin', `
-              <img class="modal-img" src="${people[i].picture.thumbnail}" alt="profile picture">
-              <h3 id="name" class="modal-name cap">${people[i].name.last} ${people[i].name.first}</h3>
-              <p class="modal-text">${people[i].email}</p>
-              <p class="modal-text cap">${people[i].location.city}, ${people[i].location.state}</p>
-              <hr>
-              <p class="modal-text">${people[i].cell}</p>
-              <p class="modal-text">${people[i].location.street.number} ${people[i].location.street.name}, ${people[i].location.city}, ${people[i].location.state} ${people[i].location.postcode}</p>
-              <p class="modal-text">${people[i].dob.date.split('-')[1]}/${people[i].dob.date.split('-')[2][0]+people[i].dob.date.split('-')[2][1]}/${people[i].dob.date.split('-')[0]}</p>
-          `)
+          || event.target.src === `${people[i].picture.thumbnail}` || event.target.className === "card-info-container" && event.target.firstElementChild.textContent === `${people[i].name.last} ${people[i].name.first}`){
+            let modalInfo = document.querySelector('.modal-info-container');
+            modalInfo.innerHTML = '';
+            modalInfo.insertAdjacentHTML('afterbegin', `
+                <img class="modal-img" src="${people[i].picture.thumbnail}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${people[i].name.last} ${people[i].name.first}</h3>
+                <p class="modal-text">${people[i].email}</p>
+                <p class="modal-text cap">${people[i].location.city}, ${people[i].location.state}</p>
+                <hr>
+                <p class="modal-text">${people[i].cell}</p>
+                <p class="modal-text">${people[i].location.street.number} ${people[i].location.street.name}, ${people[i].location.city}, ${people[i].location.state} ${people[i].location.postcode}</p>
+                <p class="modal-text">${people[i].dob.date.split('-')[1]}/${people[i].dob.date.split('-')[2][0]+people[i].dob.date.split('-')[2][1]}/${people[i].dob.date.split('-')[0]}</p>
+            `)
         }
       };
       modalWindow.addEventListener('click', e => {
